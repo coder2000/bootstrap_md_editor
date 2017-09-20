dcf177d85d02c7433d43dacda02a098ef1c24421
Dieter Lunn <coder2000@gmail.com>
2017-09-20 16:08:35 -0500

    Add text area to editor

--- app/helpers/bootstrap_md_editor/view_helpers.rb
+++ app/helpers/bootstrap_md_editor/view_helpers.rb
@@ -1,7 +1,8 @@
 module BootstrapMdEditor
   module ViewHelpers
-    def bootstrap_md_editor
+    def bootstrap_md_editor(model, field)
       render 'bootstrap_md_editor/toolbar'
+      render 'bootstrap_md_editor/text_area', model: model, field: field
     end
   end
 end
